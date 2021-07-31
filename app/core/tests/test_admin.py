666c3eba4cdbc2935b79d5d193f0a52102e5ede8
Oscar Javier Moreno Rey <ojmorenor@gmail.com>
2021-07-31 09:26:04 +0200

    Use custom user in admin edit user page

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -20,7 +20,15 @@ class AdminSiteTests(TestCase):
 
     def test_users_listed(self):
         url = reverse("admin:core_user_changelist")
+
         response = self.client.get(url)
 
         self.assertContains(response, self.user.name)
         self.assertContains(response, self.user.email)
+
+    def test_edit_user_page(self):
+        url = reverse("admin:core_user_change", args=[self.user.id])
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
